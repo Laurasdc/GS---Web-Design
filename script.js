97ed9e5d72e2e494d16b92be39da9c9af2c65dd4
Laurasdc <laurasouzacarvalho82@gmail.com>
2024-06-05 20:20:40 -0300

    Commit da branch1 - Acréscimo de funções

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-alert("Bem-vindo ao BlueGuard!");
+window.onload = function() {
+    alert("Bem-vindo ao BlueGuard!");
+    document.body.style.opacity = "1";
+}
+document.getElementById('loginImage').addEventListener('click', function() {
+        document.getElementById('login').style.display = 'block';
+    });
+
+document.getElementById('loginButton').addEventListener('click', function(event) {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    if (username === "" || password === "") {
+        alert("Por favor, preencha todos os campos.");
+        event.preventDefault();
+    }
+});
+
+var menuItems = document.querySelectorAll('.cabecalho-menu-item');
+        menuItems.forEach(function(menuItem) {
+            menuItem.addEventListener('click', function(event) {
+                event.preventDefault();
+                var target = document.querySelector(this.getAttribute('href'));
+                target.scrollIntoView({ behavior: 'smooth' });
+            });
+        });
